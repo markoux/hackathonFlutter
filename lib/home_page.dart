6443c926d132403883401fef845f7ef75af34a00
Christos Markou <christos.markou@sp-con.de>
2019-06-01 13:28:41 +0200

    tabs not working

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,69 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  List<String> tabNames = const <String>[
+    'Page1',
+    'Page2',
+    'Page3',
+  ];
+
+  @override
+  void initState() {
+    _tabController = TabController(length: tabNames.length, vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery
+        .of(context)
+        .size
+        .width;
     return Scaffold(
-      appBar: AppBar(title: Text('Home')),
+      drawer: Drawer(
+        child: Container(),
+      ),
+      appBar: AppBar(
+        title: Container(
+            height: 30,
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: 'Search',
+                  labelStyle: TextStyle(textBaseline: TextBaseline.alphabetic),
+                  prefixIcon: Icon(Icons.search),
+                  isDense: true,
+                  filled: true,
+                  fillColor: Colors.white),
+            )),
+        bottom: TabBar(
+          isScrollable: false,
+          tabs: new List.generate(tabNames.length, (index) {
+            return new Tab(text: tabNames[index].toUpperCase());
+          }),
+          onTap: (i) {
+            FocusScope.of(context).requestFocus(FocusNode());
+          },
+          controller: _tabController,
+        ),
+      ),
+      body: Container(
+      ),
     );
   }
 }
